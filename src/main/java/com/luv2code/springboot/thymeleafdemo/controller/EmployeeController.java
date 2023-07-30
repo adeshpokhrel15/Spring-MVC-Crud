75b0410c54bd659c1cdbda2eaaa7961a0a01cef4
adeshpokhrel <apokhrel@inventechgroup.com>
2023-07-30 18:32:01 +0545

    final one with crud operation

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -55,6 +52,26 @@ public class EmployeeController {
 		return "redirect:/employees/list";
 
 	}
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employee") int theId, Model theModel){
+		// get the employee from the service
+		Employee theEmployee= employeeService.findById(theId);
+
+		// set the employee in the model to prepopulate the form
+			theModel.addAttribute("employee", theEmployee);
+		// send over to our form
+		return "employees/employee-form";
+	}
+	@GetMapping("/delete")
+	public String delete(@RequestParam("employeeId") int theId){
+		// delete the employee
+		employeeService.deleteById(theId);
+
+		// redirect to the employee/list
+		return "redirect:/employees/list";
+	}
+
 }
 
 
