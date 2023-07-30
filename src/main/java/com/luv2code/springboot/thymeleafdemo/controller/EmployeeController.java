11abb319daf0421051378907361e0eaba92a3ab2
adeshpokhrel <apokhrel@inventechgroup.com>
2023-07-30 17:41:01 +0545

    mvc add

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -35,12 +37,25 @@ public class EmployeeController {
 
 		return "employees/list-employees";
 	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		Employee theEmployee = new Employee();
+		theModel.addAttribute("employee", theEmployee);
+		return "employees/employee-form";
+	}
+
+	@PostMapping("/save")
+	// form data is passed in here
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		// save the employee
+		employeeService.save(theEmployee);
+
+		// use a redirect to prevent duplicate submission
+		return "redirect:/employees/list";
+
+	}
 }
-
-
-
-
-
 
 
 
